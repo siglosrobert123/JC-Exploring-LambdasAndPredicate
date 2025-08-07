6f7625fc82a443e3495dfc309f6771c2a205a39e
Robert <johnrobert.siglos@gcash.com>
2025-08-07 14:41:59 +0800

    Final Submission, Exploring Lambdas and Predicates

---
+++ README.md
@@ -0,0 +1,6 @@
+# Exploring Lambdas and Predicates
+
+## John Robert H. Siglos
+
+### Sample Output: <br>
+![img.png](img.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,42 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Predicate<String> isLong = s -> s.length() >10;
+        String str1 = "short";
+        String str2 = "This is a very long string";
+        System.out.println("Is '" + str1 + "' long? " + isLong.test(str1));
+        System.out.println("Is '" + str2 + "' long? " + isLong.test(str2));
+
+        List<String> callSigns = new ArrayList<>();
+        callSigns.add("Alpha");
+        callSigns.add("Bravo");
+        callSigns.add("Archangel");
+        callSigns.add("Echo");
+        callSigns.add("Avenger");
+        Predicate<String> startsWithA = s -> s.startsWith("A");
+        filterAndPrint(callSigns, startsWithA, "Call signs starting with A");
+
+        Predicate<String> hasLengthGreaterThan5 = s -> s.length()>5;
+        Predicate<String> combined =startsWithA.and(hasLengthGreaterThan5);
+        filterAndPrint(callSigns, combined, "Starts with 'A' AND length > 5");
+
+        Predicate<String> doesNotStartWithA = startsWithA.negate();
+        filterAndPrint(callSigns, doesNotStartWithA, "does NOT start with 'A'");
+
+    }
+
+    public static void filterAndPrint(List<String> list, Predicate<String> predicate, String description){
+        System.out.println("--- " + description + " ---");
+        for(String item : list){
+            if(predicate.test(item)){
+                System.out.println(item);
+            }
+        }
+        System.out.println();
     }
 }
